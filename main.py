a75155bff7d9c76431ce6c7c2e359e2feec68a40
19Duartd <136071643+19Duartd@users.noreply.github.com>
2023-06-21 10:51:40 +0000

    Ty being a piece of shit

--- main.py
+++ main.py
@@ -16,60 +16,85 @@ info_panels = [] #Location of the information panels
 workers = [] #Location of the worker aliens
 #Procedure declarations
 def load_module():
- global module, possible_moves
- possible_moves = get_modules_from(module)
- output_module()
+    global module, possible_moves
+    possible_moves = get_modules_from(module)
+    output_module()
   
 def get_modules_from(module):
- moves = []
- text_file = open("Charles_Darwin/module" + str(module) + ".txt", "r")
- for counter in range(0,4):
-  move_read = text_file.readline()
-  move_read = int(move_read.strip())
-  if move_read != 0:
-    moves.append(move_read)
-  text_file.close()
- return moves
+    moves = []
+    text_file = open("Charles_Darwin/module" + str(module) + ".txt", "r")
+    for counter in range(0,4):
+        move_read = text_file.readline()
+        move_read = int(move_read.strip())
+        if move_read != 0:
+            moves.append(move_read)
+    text_file.close()
+    return moves
 
 def output_module():
- global module
- print()
- print("-----------------------------------------------------------------")
- print()
- print("You are in module",module)
- print()
+    global module
+    print()
+    print("-----------------------------------------------------------------")
+    print()
+    print("You are in module",module)
+    print()
 def output_moves():
- global possible_moves
- print()
- print("From here you can move to modules: | ",end='')
- for move in possible_moves:
-  print(move,'| ',end='')
-  print()
-
+    global possible_moves
+    print()
+    print("From here you can move to modules: | ",end='')
+    for move in possible_moves:
+        print(move,'| ',end='')
+    print()
 def get_action():
- global module, last_module, possible_moves
- valid_action = False
- while valid_action == False:
-  print("What do you want to do next ? (MOVE, SCANNER)")
-  action = input(">")
-  if action == "MOVE":
-    move = int(input("Enter the module to move to: "))
-  if move in possible_moves:
-    valid_action = True
-    last_module = module
-    module = move
-  else:
-    print("The module must be connected to the current module.")
+    global module, last_module, possible_moves
+    valid_action = False
+    while valid_action == False:
+        print("What do you want to do next ? (MOVE, SCANNER)")
+        action = input(">")
+        if action == "MOVE":
+            move = int(input("Enter the module to move to: "))
+            if move in possible_moves:
+                valid_action = True
+                last_module = module
+                module = move
+            else:
+                print("The module must be connected to the current module.")
+
+def spawn_npcs():
+  global num_modules, queen, vent_shafts, info_panels, workers
+  module_set = []
+  for counter in range(2,num_modules + 1):
+    module_set.append(counter)
+  random.shuffle(module_set)
+  i = 0
+  queen = module_set[i]
+  for counter in range(0,3):
+    i=i+1
+    vent_shafts.append(module_set[i])
+  for counter in range(0,2):
+    i=i+1
+    info_panels.append(module_set[i])
+
+  for counter in range(0,3):
+    i=i+1
+    workers.append(module_set[i])
+
 
 #Main program starts here
-
+spawn_npcs()
+print("Queen alien is located in module:",queen)
+print("Ventilation shafts are located in modules:",vent_shafts)
+print("Information panels are located in modules:",info_panels)
+print("Worker aliens are located in modules:",workers)
 while alive and not won:
- load_module()
- if won == False and alive == True:
-  output_moves()
-  get_action()
+    load_module()
+    if won == False and alive == True:
+        output_moves()
+        get_action()
+        
 if won == True:
     print("The queen is trapped and you burn it to death with your flamethrower.")
     print("Game over. You win!")
 if alive == False:
     print("The station has run out of power. Unable to sustain life support, you die.")
+
